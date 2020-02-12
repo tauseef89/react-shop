fe27d978428efd6d316fd6a9af175583ee53c27e
tauseef89 <tauseef.n89@gmail.com>
2020-02-12 17:15:32 +0530

    part 3 useState

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ProductList = () => {
+  const [products, setProducts] = useState([
+    {
+      title: "Product title 1",
+      price: "200"
+    },
+    {
+      title: "Product title 2",
+      price: "300"
+    }
+  ]);
   return (
     <ul className="product-list">
-      <li>
-        <h3>Product title 1</h3>
-        <p>Price: 200</p>
-      </li>
-      <li>
-        <h3>Product title 2</h3>
-        <p>Price: 300</p>
-      </li>
+      {products.map(product => {
+        return (
+          <li>
+            <h3>{product.title}</h3>
+            <p>Price: {product.price}</p>
+          </li>
+        );
+      })}
     </ul>
   );
 };
