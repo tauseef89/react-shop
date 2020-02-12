a27af45f197f64f7d8fabc6c031ef731e77d78b4
tauseef89 <tauseef.n89@gmail.com>
2020-02-12 18:28:13 +0530

    part 6 useEffect

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ProductInfo from "./ProductInfo";
 import NewProductForm from "./NewProductForm";
 
@@ -15,18 +15,29 @@ const ProductList = () => {
       price: "300"
     }
   ]);
+  const [age, setAge] = useState(20);
+
   const addProduct = (title, price) => {
     setProducts([
       ...products,
       { title: title, price: price, id: Math.random() }
     ]);
   };
+  useEffect(() => {
+    console.log("useEffect callback", products);
+  }, [products]);
+
+  useEffect(() => {
+    console.log("useEffect callback", age);
+  }, [age]);
   return (
     <ul className="product-list">
       {products.map(product => {
         return <ProductInfo product={product} key={product.id} />;
       })}
       <NewProductForm addProduct={addProduct} />
+
+      <button onClick={() => setAge(age + 1)}>Add 1 to age: {age}</button>
     </ul>
   );
 };
