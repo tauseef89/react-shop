3ab8fb18379c66e9b594827233257c88ff9f32f0
tauseef89 <tauseef.n89@gmail.com>
2020-02-12 17:01:06 +0530

    part 2 components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import ProductList from "./components/ProductList";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <h2>My Shop</h2>
+      <Header />
+      <ProductList />
     </div>
   );
 }
---
+++ src/components/ProductList.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ProductList = () => {
+  return (
+    <ul className="product-list">
+      <li>
+        <h3>Product title 1</h3>
+        <p>Price: 200</p>
+      </li>
+      <li>
+        <h3>Product title 2</h3>
+        <p>Price: 300</p>
+      </li>
+    </ul>
+  );
+};
+
+export default ProductList;
